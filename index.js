8d80e0dd9be86a0fd8987443de362628b0877edd
RobertDavis1408 <125427151+RobertDavis1408@users.noreply.github.com>
2023-05-29 13:32:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -15,9 +15,11 @@ const shoppingListEl = document.getElementById("shopping-list")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
+    if(inputValue){
+
     
     push(shoppingListInDB, inputValue)
-    
+    } 
     clearInputFieldEl()
 })
 
